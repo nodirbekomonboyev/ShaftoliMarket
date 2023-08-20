346379a138302d1a8a9652a049870f9ca977ce65
nodirbekomonboyev <omonboevnodir77@gmail.com>
2023-08-20 14:13:22 +0500

    Initial commit

---
+++ src/main/java/uz/shaftolimarket/ShaftoliMarketApplication.java
@@ -0,0 +1,13 @@
+package uz.shaftolimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaftoliMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaftoliMarketApplication.class, args);
+    }
+
+}
