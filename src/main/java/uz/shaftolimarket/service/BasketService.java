306b50cff9a1b999c9417f4695ae3182216c5ac6
nodirbekomonboyev <omonboevnodir77@gmail.com>
2023-08-23 23:56:30 +0500

    add createBasket function

---
+++ src/main/java/uz/shaftolimarket/service/BasketService.java
@@ -0,0 +1,49 @@
+package uz.shaftolimarket.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uz.shaftolimarket.model.BasketEntity;
+import uz.shaftolimarket.model.UserEntity;
+import uz.shaftolimarket.model.UserRole;
+import uz.shaftolimarket.repository.BasketRepository;
+import uz.shaftolimarket.repository.UserRepository;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class BasketService {
+
+    private final BasketRepository basketRepository;
+
+    public UUID save(UUID productId, Integer count, UUID userId){
+
+        BasketEntity basket = BasketEntity.builder()
+                .userId(userId)
+                .productId(productId)
+                .count(count)
+                .build();
+
+        basketRepository.save(basket);
+
+        return userId;
+    }
+    public UUID save(UUID productId, Integer count){
+        UserEntity user = UserEntity.builder()
+                .id(UUID.randomUUID())
+                .role(UserRole.USER)
+                .build();
+
+        BasketEntity basket = BasketEntity.builder()
+                .userId(user.getId())
+                .productId(productId)
+                .count(count)
+                .build();
+
+        basketRepository.save(basket);
+
+        return user.getId();
+    }
+
+
+}
