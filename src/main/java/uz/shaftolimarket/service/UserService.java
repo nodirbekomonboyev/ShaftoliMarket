06e7e1ab2b750e4616ef00c4e4ddc194364aa6a6
nodirbekomonboyev <omonboevnodir77@gmail.com>
2023-08-26 14:00:37 +0500

    send Code

--- src/main/java/uz/shaftolimarket/service/UserService.java
+++ src/main/java/uz/shaftolimarket/service/UserService.java
@@ -4,7 +4,11 @@ package uz.shaftolimarket.service;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import uz.shaftolimarket.dto.UserDTO;
+import uz.shaftolimarket.model.UserEntity;
 import uz.shaftolimarket.repository.UserRepository;
+
+import static uz.shaftolimarket.model.UserRole.USER;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +18,11 @@ public class UserService {
     private final ModelMapper modelMapper;
 
 
-    public Boolean verifyByEmail(String email, String code){
-        return false;
+    public UserEntity save(UserDTO userDTO){
+        UserEntity user = modelMapper.map(userDTO, UserEntity.class);
+        user.setRole(USER);
+        user.setVerified(false);
+        emailCodeService.save(user.getEmail());
+        return user;
     }
-
-
 }
